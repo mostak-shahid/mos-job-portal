bd02c75ef9ab04679bfbb7007bd89bc28005d1d8
Md. Mostak Shahid <mostak.shaid@gmail.com>
2019-01-22 11:36:37 +0600

    Create mos-jportal.php

---
+++ mos-jportal.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Mos Plugin
+Description: Base of future plugin
+Version: 0.0.1
+Author: Md. Mostak Shahid
+*/
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+  exit; // Exit if accessed directly.
+}
+
+// Define MOS_JPORTAL_FILE.
+if ( ! defined( 'MOS_JPORTAL_FILE' ) ) {
+  define( 'MOS_JPORTAL_FILE', __FILE__ );
+}
+// Define MOS_JPORTAL_SETTINGS.
+if ( ! defined( 'MOS_JPORTAL_SETTINGS' ) ) {
+  //define( 'MOS_JPORTAL_SETTINGS', admin_url('/edit.php?post_type=post_type&page=plugin_settings') );
+  define( 'MOS_JPORTAL_SETTINGS', admin_url('/options-general.php?page=mos_jportal_settings') );
+}
+$mos_jportal_option = get_option( 'mos_jportal_option' );
+$plugin = plugin_basename(MOS_JPORTAL_FILE); 
+require_once ( plugin_dir_path( MOS_JPORTAL_FILE ) . 'mos-jportal-functions.php' );
+require_once ( plugin_dir_path( MOS_JPORTAL_FILE ) . 'mos-jportal-settings.php' );
+//require_once ( plugin_dir_path( MOS_JPORTAL_FILE ) . 'custom-settings.php' );
+
+require_once('plugins/update/plugin-update-checker.php');
+$pluginInit = Puc_v4_Factory::buildUpdateChecker(
+  'https://raw.githubusercontent.com/mostak-shahid/update/master/mos-jportal.json',
+  MOS_JPORTAL_FILE,
+  'mos-jportal'
+);
+
+
+register_activation_hook(MOS_JPORTAL_FILE, 'mos_jportal_activate');
+add_action('admin_init', 'mos_jportal_redirect');
+ 
+function mos_jportal_activate() {
+    $mos_jportal_option = array();
+    // $mos_jportal_option['mos_login_type'] = 'basic';
+    // update_option( 'mos_jportal_option', $mos_jportal_option, false );
+    add_option('mos_jportal_do_activation_redirect', true);
+}
+ 
+function mos_jportal_redirect() {
+    if (get_option('mos_jportal_do_activation_redirect', false)) {
+        delete_option('mos_jportal_do_activation_redirect');
+        if(!isset($_GET['activate-multi'])){
+            wp_safe_redirect(MOS_JPORTAL_SETTINGS);
+        }
+    }
+}
+
+// Add settings link on plugin page
+function mos_jportal_settings_link($links) { 
+  $settings_link = '<a href="'.MOS_JPORTAL_SETTINGS.'">Settings</a>'; 
+  array_unshift($links, $settings_link); 
+  return $links; 
+} 
+add_filter("plugin_action_links_$plugin", 'mos_jportal_settings_link' );
