acd36d29b8b34c6950aacbc711b908011de8e003
Md. Mostak Shahid <mostak.shaid@gmail.com>
2019-01-22 16:59:19 +0600

    Update page-welcome.php

--- page-welcome.php
+++ page-welcome.php
@@ -3,7 +3,10 @@ if (!is_user_logged_in()) {
 	wp_redirect( home_url() );
 	exit;
 } else {
-	echo get_current_user_id();
+	$user_id =  get_current_user_id();
+	$user_meta = get_userdata($user_id);
+	$user_roles = $user_meta->roles;
+	var_dump($user_roles[0]);
 }
 require_once ( plugin_dir_path( MOS_JPORTAL_FILE ) . 'header-jportal.php' );
 ?>
