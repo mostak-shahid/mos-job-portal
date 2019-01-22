080491f694cdfcb7ae12313e2f0fa0ff57df28f4
Md. Mostak Shahid <mostak.shaid@gmail.com>
2019-01-22 16:55:50 +0600

    0.0.1

--- page-welcome.php
+++ page-welcome.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Bootstrap Example</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="author" content="Md. Mostak Shahid">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-</head>
-<body>
+<?php
+if (!is_user_logged_in()) {
+	wp_redirect( home_url() );
+	exit;
+} else {
+	echo get_current_user_id();
+}
+require_once ( plugin_dir_path( MOS_JPORTAL_FILE ) . 'header-jportal.php' );
+?>
+
 
 	<div class="container">
 		<h1>My First Bootstrap Page</h1>
@@ -18,5 +15,4 @@
 		<p>The .container class provides a responsive fixed width container.</p>           
 	</div>
 
-</body>
-</html>
+<?php require_once ( plugin_dir_path( MOS_JPORTAL_FILE ) . 'footer-jportal.php' ); ?>
